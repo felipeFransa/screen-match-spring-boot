0d7080f9303a743e690abdce4c870b74d4e9d946
Felipe Fransa <antoniofelipesilva@live.com>
2024-04-29 18:14:34 -0300

    first commit

---
+++ src/main/java/br/com/alura/sreenMatch/SreenMatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.sreenMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreenMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreenMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/sreenMatch/SreenMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.sreenMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SreenMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
